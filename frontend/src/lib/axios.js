7b5c9f8893bc6960ab33a948aa40215b12df4274
Vishal Singh <vish.singh3020@gmail.com>
2025-05-18 13:55:19 +0530

    axios change

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://real-time-chat-app-jd8s.onrender.com",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
   withCredentials: true,
 });
