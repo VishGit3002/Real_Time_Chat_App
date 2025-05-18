9834a493b651e8a0ba8fd2fdc7ef2fc1fda85dd5
Vishal Singh <vish.singh3020@gmail.com>
2025-05-18 13:31:33 +0530

    deployment checking

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5173 || https://real-time-chat-app-six-snowy.vercel.app/login",
     credentials: true,
   })
 );
