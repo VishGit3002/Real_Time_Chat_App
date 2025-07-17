b403473fa648c5a728f1f44333ea2561b89edc28
Vishal Singh <vish.singh3020@gmail.com>
2025-07-17 18:35:18 +0530

    complete

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-<<<<<<< HEAD
   baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
-=======
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
->>>>>>> dfd8ed29047d7aa2edda655ec22020a519f1c7a9
   withCredentials: true,
 });
